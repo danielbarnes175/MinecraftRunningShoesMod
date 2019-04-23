ff5cd6c534969e0acf4c881beb639eed1d51d25a
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2019-04-22 20:27:22 -0500

    Add speed effect on item equip

--- java/bballdaniel3/runningshoesmod/Items/ArmorBase.java
+++ java/bballdaniel3/runningshoesmod/Items/ArmorBase.java
@@ -4,8 +4,13 @@ import bballdaniel3.runningshoesmod.Main;
 import bballdaniel3.runningshoesmod.init.ModItems;
 import bballdaniel3.runningshoesmod.util.IHasModel;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 
 public class ArmorBase extends ItemArmor implements IHasModel {
 
@@ -20,6 +25,11 @@ public class ArmorBase extends ItemArmor implements IHasModel {
 		ModItems.ITEMS.add(this);
 	}
 
+	@Override
+	public void onArmorTick(World world, EntityPlayer player, ItemStack armor) {
+		player.addPotionEffect(new PotionEffect(Potion.REGISTRY.getObjectById(1), 0, 1));
+
+	}
 	@Override
 	public void registerModels() {
 		Main.proxy.registerItemRenderer(this, 0, "inventory");
