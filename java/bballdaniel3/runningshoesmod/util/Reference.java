720f821e1d2f8902bf87998d26af5afdebb8a6e5
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2019-04-30 13:07:47 -0500

    Update version

--- java/bballdaniel3/runningshoesmod/util/Reference.java
+++ java/bballdaniel3/runningshoesmod/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	
 	public static final String MOD_ID = "runningshoesmod";
 	public static final String NAME = "Running Shoes Mod";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "bballdaniel3.runningshoesmod.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "bballdaniel3.runningshoesmod.proxy.CommonProxy";
